584e1851f999edd96b5e103f1484c5e897941492
mark-kleppe <jsmkhero9@gmail.com>
2016-05-22 22:52:21 -0500

    Initial Commit

---
+++ Assets/Scripts/BunnyController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BunnyController : MonoBehaviour {
+
+    private Rigidbody2D _body;
+
+    public float force = 750f;
+
+	// Use this for initialization
+	void Start () {
+        _body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetButtonUp("Jump"))
+        {
+            _body.AddForce(transform.up * force);
+        }
+	}
+}
